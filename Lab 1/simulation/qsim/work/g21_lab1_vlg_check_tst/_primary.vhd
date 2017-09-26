14bf3e897d3c077c7c50c1132fca9e51ce5edde2
Kevin Sorto-Ventura <ksortoventura@gmail.com>
2017-09-26 14:53:29 -0400

    Make modulo 13 output 4 bits

--- Lab 1/simulation/qsim/work/g21_lab1_vlg_check_tst/_primary.vhd
+++ Lab 1/simulation/qsim/work/g21_lab1_vlg_check_tst/_primary.vhd
@@ -3,7 +3,7 @@ use verilog.vl_types.all;
 entity g21_lab1_vlg_check_tst is
     port(
         AeqB            : in     vl_logic;
-        Amod13          : in     vl_logic_vector(7 downto 0);
+        Amod13          : in     vl_logic_vector(3 downto 0);
         Cout            : in     vl_logic;
         floor           : in     vl_logic_vector(2 downto 0);
         \out\           : in     vl_logic_vector(7 downto 0);
