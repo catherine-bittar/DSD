fb8dc005b062686762bb2c2ed03fa787e8548ac4
Kevin Sorto-Ventura <ksortoventura@gmail.com>
2017-11-15 19:13:21 -0500

    Continue Rules

--- Lab 4/g21_rules.vhd
+++ Lab 4/g21_rules.vhd
@@ -15,21 +15,25 @@ use lpm.lpm_components.all;
 entity g21_rules is
 	port(play_pile_top_card: in std_logic_vector(5 downto 0);
 	     card_to_play      : in std_logic_vector(5 downto 0);
-        legal_play        : out std_logic);
+		  one_button        : in std_logic;
+		  eleven_button     : in std_logic;
+        legal_play        : out std_logic;
+		  left_segment      : out std_logic_vector(6 downto 0);
+		  right_segment     : out std_logic_vector(6 downto 0));
 end g21_rules;	
 
 architecture game_rules of g21_rules is
-		signal face_top: std_logic_vector(3 downto 0);
-		signal suit_top: std_logic_vector(2 downto 0); 
-		signal face_card: std_logic_vector(3 downto 0);
-		signal suit_card: std_logic_vector(2 downto 0); 
-		signal top_val: std_logic_vector(3 downto 0); 
-		signal card_val: std_logic_vector(3 downto 0);
-		signal tot_val: std_logic_vector(3 downto 0);
+	signal face_top: std_logic_vector(3 downto 0);
+	signal suit_top: std_logic_vector(2 downto 0); 
+	signal face_card: std_logic_vector(3 downto 0);
+	signal suit_card: std_logic_vector(2 downto 0); 
+	signal top_val: std_logic_vector(5 downto 0); 
+	signal card_val: std_logic_vector(5 downto 0);
+	signal tot_val: std_logic_vector(5 downto 0);
 		
 		component g21_Modulo_13 -- begin description of modulo 13 component
 		port (A     : in std_logic_vector(5 downto 0) ;
-			   floor : out std_logic_vector(2 downto 0); -- useless?
+			   floor : out std_logic_vector(2 downto 0); 
 			   modout: out std_logic_vector(3 downto 0) );
 	   end component;
 
@@ -41,9 +45,24 @@ begin-- use g21_Modulo_13 component to assign signal values
 	card_val_assign: process(face_top, face_card, top_val, card_val, tot_val)
 	begin
 	
-	if (face_top >= "0010" and face_top <= "1010") then
-		top_val <= face_top;
-	elsif (face_top >= "1011") then
-		top_val <= "1010";
-	end if;
+		if (face_top >= "0010" and face_top <= "1010") then
+			top_val <= "00" & face_top + "000001";
+		elsif (face_top >= "1011") then
+			top_val <= "00" & "1010";
+		elsif (face_top = "0001") then
+			left_segment <= "1111001"; -- code for 1
+			right_segment <= "1001001"; -- code for 11
+			if(one_button = '0') then
+				top_val <= "0001";
+				left_segment <= "1111111"; 
+				right_segment <= "1111111"; 
+			elsif(eleven_button ='0') then
+				top_val <= "1011";
+				left_segment <= "1111111"; 
+				right_segment <= "1111111"; 
+			end if;
+		end if;
 		
+		tot_val <= top_val + card_val;
+	end process;
+end architecture;		
