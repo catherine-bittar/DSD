099802df79b3d33806a0c7f3207e06f07d8e5022
Kevin Sorto-Ventura <ksortoventura@gmail.com>
2017-11-27 19:00:11 -0500

    Begin Deal FSM

--- Lab 5/Deal_FSM.vhd
+++ Lab 5/Deal_FSM.vhd
@@ -15,8 +15,16 @@ use lpm.lpm_components.all;
 
 entity Deal_FSM is
 	port(Request_Deal : in std_logic;
+		  Stop         : in std_logic;
+		  INIT         : in std_logic;
 		  reset        : in std_logic;
 		  Clock        : in std_logic;
+		  Dealer_legal : in std_logic; -- TODO: take legal_play output from dealer rules module
+		  Player_legal : in std_logic; -- TODO: similarly like above
+		  Dealer_total : in std_logic_vector (5 downto 0);
+		  Player_total : in std_logic_vector (5 downto 0);
+		  Dealer_high  : in std_logic; -- TODO: 1 when dealer sum > player sum (would require a comparator module)
+		  Player_high  : in std_logic; -- TODO: similarly like above
 		  Rand_Enable  : out std_logic;
 		  Dealer_Enable: out std_logic;
 		  Player_Enable: out std_logic);
@@ -24,47 +32,123 @@ end Deal_FSM;
 
 architecture deal_machine of Deal_FSM is
 
-signal count: std_logic_vector(2 downto 0); 
+type state is (waiting, ready, dealer_wait, player_wait, dealer_card1,
+               dealer_card2, player_card1, player_card2, dealer_play, 
+					dealer_fc, player_fc, player_play, dealer_en,player_en,
+					dealer_test, player_test, dealer_win, player_win, compare); 
+
+signal present_state, next_state: state;
 
 begin
-
-		  
-	rand: process (reset, Clock, Request_Deal)
+	  
+	fsm: process (reset, Clock)
 	begin 
-	
 		if (reset = '1') then
-			Rand_Enable <= '0';
-			Dealer_Enable <= '0';
-			Player_Enable <= '0';
+			present_state <= waiting;
 		elsif (Clock = '1' and Clock'event) then
-			Rand_Enable <= Request_Deal;
+			present_state <= next_state;
 		end if;
-		
-	end process;
+	end process fsm;
 	
-	dealer_player: process(reset, Clock, Request_Deal)
+	state_assign: process (present_state, INIT, Stop, Request_Deal, Dealer_legal, Player_legal)
 	begin
-	
-	if (Clock = '1' and Clock'event) then
-		Clock'event;
-		wait until Clock = '1';
-		Dealer_Enable <= '1';
-		wait until Clock = '1';
-		Dealer_Enable <= '0';
-		wait until Clock = '1';
-		Player_Enable <= '1';
-		wait until Clock = '1';
-		Player_Enable <= '0';
-		wait until Clock = '1';
-		Dealer_Enable <= '1';
-		wait until Clock = '1';
-		Dealer_Enable <= '0';
-		wait until Clock = '1';
-		Player_Enable <= '1';
-		wait until Clock = '1';
-		Player_Enable <= '0';
-	end if;
-	
-	end process;
+		case present_state is
+			
+			when waiting =>
+				if (INIT = '1') then
+					next_state <= ready;
+				else
+					next_state <= waiting;
+				end if;
+				
+			when ready =>
+				next_state <= dealer_card1;
+			
+			when dealer_card1 =>
+				next_state <= player_card1;
+				
+			when player_card1 =>
+				next_state <= dealer_card2;
+				
+			when dealer_card2 =>
+				next_state <= player_card2;
+				
+			when player_card2 => 
+				next_state <= dealer_fc;
+			
+			when dealer_fc =>
+				if (Dealer_total = "010101") then
+					next_state <= Dealer_win;
+				else
+					next_state <= player_fc;
+				end if;	
+				
+			when player_fc =>
+				if (Player_total = "010101") then
+					next_state <= Player_win;
+				else
+					next_state <= player_wait;
+				end if;
+			
+			when player_wait =>
+				if (stop = '1') then
+					next_state <= dealer_wait;
+				elsif (Request_Deal = '1') then
+					next_state <= player_play;
+				end if;
+				
+			when player_play =>
+				next_state <= player_en;
+				
+			when player_en =>
+				next_state <= player_test;
+				
+			when player_test =>
+				if (Player_legal = '1' and Player_total /= "010101") then
+					next_state <= dealer_wait;
+				elsif (Player_legal = '1' and Player_total = "010101") then
+					next_state <= player_win;
+				elsif (Player_legal = '0') then
+					next_state <= dealer_win;
+				end if;
+				
+			when dealer_wait =>
+				if (Dealer_total <= "010000") then
+					next_state <= dealer_play;
+				elsif (Dealer_total > "010000" and Dealer_total /= "010101") then 
+					next_state <= compare;
+				elsif (Dealer_total = "010101") then
+					next_state <= dealer_win;
+				end if;
+				
+			when dealer_play =>
+				next_state <= dealer_en;
+				
+			when dealer_en =>
+				next_state <= dealer_test;
+				
+			when dealer_test =>
+				next_state <= dealer_wait;
+			 
+			when compare =>
+				if (Dealer_high = '1') then
+					next_state <= dealer_win;
+				elsif(Player_high = '1') then
+					next_state <= player_win;
+				end if;
+				
+			when dealer_win =>
+				if (INIT = '1') then
+					next_state <= ready;
+				end if;
+				
+			when player_win =>
+				if (INIT = '1') then
+					next_state <= ready;
+				end if;
+					
+		
+		end case;
+	end process state_assign;
 	
 end architecture;
